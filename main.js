09fe757d95a8079c60b6c3ac0cb13306f3bf9db0
Luís Mendes <106193347+luis-mendes-95@users.noreply.github.com>
2024-06-14 19:51:36 -0300

    update

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ window.addEventListener('load', function() {
             .catch(error => console.log(error));
 
             /** SCENES MANAGEMENT */
-            this.currentScene = new Scene2(this);
+            this.currentScene = new Scene0(this);
         }
 
         changeScene(newScene) {
