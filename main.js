5e354c17f07b3c40bfee0d9ebb32b5ed21f14f36
luis-mendes-95 <luis.mendes.95@hotmail.com>
2024-07-12 18:01:51 -0300

    update

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ import { Scene4 } from './src/customClasses/scenes/scene4/scene4.js';
 import { Scene5 } from './src/customClasses/scenes/scene5/scene5.js';
 import { Scene6 } from './src/customClasses/scenes/scene6/scene6.js';
 import { Scene7 } from './src/customClasses/scenes/scene7/scene7.js';
+import { Scene8 } from './src/customClasses/scenes/scene8/scene8.js';
 
 /** AFTER EVERYTHING LOADS, IT WILL RUN */
 window.addEventListener('load', function() {
@@ -65,7 +66,7 @@ window.addEventListener('load', function() {
             .catch(error => console.log(error));
 
             /** SCENES MANAGEMENT */
-            this.currentScene = new Scene7(this);
+            this.currentScene = new Scene8(this);
             
         }
 
